c1d249f70fdd4e7d6704e26c4df0a786f09eeeb9
asif <asahmed8890@gmail.com>
2023-06-09 21:30:41 +0600

    Add files via upload

--- codeforces/158A.py
+++ codeforces/158A.py
@@ -1,20 +1,4 @@
-n, k = map(int, input().strip().split())
-a = list(map(int, input().strip().split()))
-
-li = [x for x in a if x>0 and x>=a[k-1]]
-print(li)
-print(*(0<v>=a[k-1]for v in a))
-print(sum([]))
-
-
 inp = lambda: map(int, input().strip().split())
-n, k = inp()
+_, k = inp()
 a = list(inp())
-
-
-
-
-i=lambda:[*map(int,input().split())]
-a,k=i()
-l=i()
-print(sum(0<v>=l[k-1]for v in l))
+print(sum(0 < x >= a[k - 1] for x in a))
