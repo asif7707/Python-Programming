e01bfaebcfd5254456c70fcadfcae375dadd4a01
asif <asahmed8890@gmail.com>
2023-06-10 22:17:17 +0600

    Add files via upload

--- codeforces/testcase.py
+++ codeforces/testcase.py
@@ -1,15 +1,2 @@
-import numpy as np
-
-# Create a 5x5 matrix
-matrix = np.array([[0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0],
-                   [0, 1, 0, 0, 0],
-                   [0, 0, 0, 0, 0]])
-
-# Find the position of the 1 value
-position = np.where(matrix == 1)
-
-# Print the position
-print(position)
-print(position[0][0], position[1][0])
+import re
+print(int(input()) - len(re.sub(r'(.)\1+', r'\1', input())))
