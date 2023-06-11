4dcdcc98d09bcc217ad837694ec54cd6083f7757
asif <asahmed8890@gmail.com>
2023-06-11 20:32:19 +0600

    Add files via upload

---
+++ codeforces/617A.py
@@ -0,0 +1,6 @@
+import math
+print(math.ceil(int(input())/5))
+
+# To determine the minimum number of steps we can divide
+# the distance x by the maximum distance which is 5 and then
+# round up the result to the nearest integer
